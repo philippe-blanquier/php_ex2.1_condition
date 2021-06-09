b335fa6b44555beb549cd0c125ed815cc0c7ad16
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 14:31:07 +0200

    evo

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
-<!DOCTYPE html>
-<html lang="fr">
+<html lang='fr'>
 	<head>
-        <title>PHP ex2.1 condition</title>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>PHP ex 2.1 contition</title>
     </head>
     <body >
-        <?php
-        $age=21;
-        if ($age >= 18)
-        {
-            echo ("Vous etes majeur");
-        }
-        else
-        {
-            echo ("Vous etes mineur");
-        }
-       ?>
+        <p>
+            <?php
+            $age=21;
+            if ($age >= 18)
+            {
+                echo ('Vous etes majeur');
+            }
+            else
+            {
+                echo ('Vous etes mineur');
+            }
+           ?>
+       </p>
     </body>
 </html>
